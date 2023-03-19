dbe92592b3d20899d12c6ea279611e27f9d4a853
sahilCoder077 <125252945+sahilCoder077@users.noreply.github.com>
2023-03-19 11:23:21 +0530

    Create missmach.cpp
    missmatch word

---
+++ missmach.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;  
+int main() {
+	
+	int T;
+	cin>>T;
+    	   
+	while(T--){
+	       string M;
+	       cin>>M;
+	       string N;
+	       cin>>N;
+	       
+         for(int i=0;i<5;i++){
+             
+               if(M[i]==N[i]){
+                   
+                    cout<<'G';
+
+             }else{
+                    cout<<'B';
+                    
+               }
+         }   
+         cout<<endl;
+	}	
+	return 0;
+}
+
+
+/* match miss match 
+3
+ABCDE
+EDCBA
+ROUND
+RINGS
+START
+STUNT
+
+BBGBB
+GBBBB
+GGBBG
+
+*/
