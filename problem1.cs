596512874993faff129ffb3276495076d4dc8ffc
sahilCoder077 <125252945+sahilCoder077@users.noreply.github.com>
2023-07-10 20:02:50 +0530

    Add files via upload
    STUDENT ID GENERATOR

---
+++ problem1.cs
@@ -0,0 +1,43 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+using System;
+
+namespace myapp{
+
+      class Program{
+          
+           public static void Main(string[] args){
+               
+      
+      Console.Write("Enter the name:");
+      string  name=Console.ReadLine();
+      
+      Console.Write("Enter The Birth date:");
+      string  BirthDate=Console.ReadLine();
+      
+      Console.Write("Enter the Branch name:");
+      string  BranchName=Console.ReadLine();
+      
+      Console.WriteLine("Enter the gender:");
+      string gender=Console.ReadLine();
+      
+      Console.Write("Enter the Course name:");
+      string  CourseName=Console.ReadLine();
+      
+      Console.Write("Enter the Year:");
+      string  CourseYear=Console.ReadLine();
+      
+      Console.WriteLine("****************\tSTUDENT ID\t******************");
+
+      Console.WriteLine("Name:"+name);
+      Console.WriteLine("Birth date:"+BirthDate);
+      Console.WriteLine("gender:"+gender);
+      Console.WriteLine("Branch Name:"+BranchName);
+      Console.WriteLine("course Name:"+CourseName);
+      Console.WriteLine("Course Year:"+CourseYear);
+      
+      
+        }
+          
+      }
+}
